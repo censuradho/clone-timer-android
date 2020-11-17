d9bdde29c05aa626ed7fd8b788d0efea6fb3e2bf
censuradho <gustavoleiteoliveira800@gmail.com>
2020-11-17 09:11:21 -0300

    movi o state do display para o timer

--- src/components/Timer/Display/index.tsx
+++ src/components/Timer/Display/index.tsx
@@ -1,35 +1,41 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState, useMemo } from 'react'
 
 import './styles.css'
 
 interface DisplayProps {
-  time: number
+  state?: number[],
+  onRemove?: () => void
 }
 
 
 function Display (props: DisplayProps) {
-  const [state, setState] = useState<number[]>([])
-  
-  const handleState = useCallback(() => {
-    setState(prev => prev.length !== 7 ? ([props.time, ...prev]) : prev)
-  }, [props.time])
+  //   const handleState = useCallback(() => {    
+  //   setState(prev => prev?.length !== 7 && props.time ? ([props.time, ...prev]) : prev)
+  // }, [props.time])
 
-  useEffect(() => {
-    handleState()
-  }, [handleState])
+  // const handleRemove = useCallback(() => {
+  //   const popLastOne = state.filter((value, index) => index !== state.length - 1)
+  //   setState(popLastOne)
+  // }, [state])
 
-  useEffect(() => {
-    console.log(state)
-  }, [state])
+  const lengthState = useMemo(() => props?.state?.length !== 0, [props?.state])
+
+  // useEffect(() => {
+  //   handleState()
+  // }, [handleState])
+
+  // useEffect(() => {
+  //   props?.onStateChange?.(state)
+  // }, [lengthState])
 
   return (
     <div className="display">
-      <div className="numbers-display">
-        <p>{state[5] ?? 0}{state[4] ?? 0}<span>h</span></p>
-        <p>{state[3] ?? 0}{state[2] ?? 0}<span>m</span></p>
-        <p>{state[1] ?? 0}{state[0] ?? 0}<span>s</span></p>
+      <div className={`numbers-display ${lengthState ? 'active' : ''}`}>
+        <p>{props?.state?.[5] ?? 0}{props?.state?.[4] ?? 0}<span>h</span></p>
+        <p>{props?.state?.[3] ?? 0}{props?.state?.[2] ?? 0}<span>m</span></p>
+        <p>{props?.state?.[1] ?? 0}{props?.state?.[0] ?? 0}<span>s</span></p>
       </div>
-      <button>
+      <button onClick={props?.onRemove}>
       <svg height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"/></svg>
       </button>
     </div>
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,31 +1,58 @@
-import React, { useMemo, useState } from 'react'
+import React, { useMemo, useState, useCallback, useEffect } from 'react'
 
 import Display from './Display'
 
 import './styles.css'
 
 function SetTimer () {
-  const [milliseconds, setMilliseconds] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+  const [state, setState] = useState<number[]>([])
+  const [milliseconds, setMilliseconds] = useState('')
 
   const values = useMemo(() => [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [])
+
+    const handleState = useCallback((value: number) => {
+      setState(prev => prev?.length !== 7 ? ([value, ...prev]) : prev)
+      setMilliseconds(prev => prev + value)
+  }, [])
+
+  const handleRemove = useCallback(() => {
+    const popLastOne = state.filter((value, index) => index !== state.length - 1)
+    setState(popLastOne)
+  }, [state])
+
+  const handlePlay = useCallback(() => {
+    
+    console.log(milliseconds)
+  }, [milliseconds])
+
+  useEffect(() => {
+    state.length > 0 ? setIsActive(true) : setIsActive(false)
+  }, [state])
 
   const renderButtons = useMemo(() => values.map(value => 
     <button 
       key={value}
       value={value}
-      onClick={() => setMilliseconds(value)}  
+      onClick={() => handleState(value)}  
     >{value}</button>
   ), [values])
 
   return (
     <div className="set-timer">
-      <Display time={milliseconds} />
+      <Display 
+        state={state}
+        onRemove={handleRemove}
+      />
       <section>
         <div className="numbers">
           { renderButtons }
         </div>
-        <button className="play">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 8.64L15.27 12 10 15.36V8.64M8 5v14l11-7L8 5z"/></svg>
+        <button 
+          className={`play ${isActive ? 'active-play' : 'inactive-play'}`}
+          onClick={handlePlay}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 8.64L15.27 12 10 15.36V8.64M8 5v14l11-7L8 5z"/></svg>
         </button>
       </section>
     </div>
