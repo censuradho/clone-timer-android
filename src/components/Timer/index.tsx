926ff003f3052c5002378faaec01eaa2b0633952
censuradho <gustavoleiteoliveira800@gmail.com>
2020-11-17 21:46:19 -0300

    add play timer

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -8,27 +8,47 @@ function SetTimer () {
   const [isActive, setIsActive] = useState(false)
   const [state, setState] = useState<number[]>([])
   const [milliseconds, setMilliseconds] = useState('')
+  const [timerInterval, setTimerInterval] = useState<NodeJS.Timeout>()
 
   const values = useMemo(() => [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [])
 
     const handleState = useCallback((value: number) => {
-      setState(prev => prev?.length !== 7 ? ([value, ...prev]) : prev)
+      if (milliseconds.length > 6) return;
+
       setMilliseconds(prev => prev + value)
-  }, [])
-
-  const handleRemove = useCallback(() => {
-    const popLastOne = state.filter((value, index) => index !== state.length - 1)
-    setState(popLastOne)
-  }, [state])
-
-  const handlePlay = useCallback(() => {
-    
-    console.log(milliseconds)
   }, [milliseconds])
 
+  console.log(milliseconds)
+  const handleRemove = useCallback(() => {
+    setMilliseconds(prevState => 
+      prevState
+        .split('')
+        .filter((value, index) => index !== 0)
+        .join('')
+      )
+    
+  }, [])
+
+const handlePlay = () => {
+  // if (milliseconds === '0') {
+  //   clearInterval(timerInterval?.ref())
+  //   return
+  // }
+  
+  setInterval(() => {
+  
+    
+    setMilliseconds(prev => (+prev - 1).toString())   
+  }, 1000)
+  
+}
+
   useEffect(() => {
-    state.length > 0 ? setIsActive(true) : setIsActive(false)
-  }, [state])
+    milliseconds.length > 0 ? setIsActive(true) : setIsActive(false)
+    console.log(milliseconds)
+
+    
+  }, [milliseconds])
 
   const renderButtons = useMemo(() => values.map(value => 
     <button 
@@ -41,7 +61,7 @@ function SetTimer () {
   return (
     <div className="set-timer">
       <Display 
-        state={state}
+        state={milliseconds}
         onRemove={handleRemove}
       />
       <section>
@@ -49,7 +69,7 @@ function SetTimer () {
           { renderButtons }
         </div>
         <button 
-          className={`play ${isActive ? 'active-play' : 'inactive-play'}`}
+          className={`${isActive ? 'active-play' : ''} play`}
           onClick={handlePlay}
         >
           <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 8.64L15.27 12 10 15.36V8.64M8 5v14l11-7L8 5z"/></svg>
